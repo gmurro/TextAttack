3149f5cd26992a332625e1996c7c9ee607ed3cd5
Giuseppe Murro <g.murro23@gmail.com>
2022-12-28 22:21:21 +0100

    update sbert metric

--- textattack/metrics/quality_metrics/sentence_bert.py
+++ textattack/metrics/quality_metrics/sentence_bert.py
@@ -1,7 +1,9 @@
 """
-USEMetric class:
+
+SBERTMetric class:
 -------------------------------------------------------
 Class for calculating SentenceBERT similarity on AttackResults
+
 """
 
 from textattack.attack_results import FailedAttackResult, SkippedAttackResult
@@ -10,19 +12,26 @@ from textattack.metrics import Metric
 
 
 class SBERTMetric(Metric):
-    def __init__(self, **kwargs):
-        self.use_obj = BERT(model_name="all-MiniLM-L6-v2", metric="cosine")
+    def __init__(
+            self,
+            model_name="stsb-mpnet-base-v2",    # best model from https://docs.google.com/spreadsheets/d/14QplCdTCDwEmTqrn1LH4yrbKvdogK4oQvYO1K1aPR5M/edit#gid=0
+            **kwargs
+            ):
+        self.model = BERT(model_name=model_name,  metric="cosine")
         self.original_candidates = []
         self.successful_candidates = []
         self.all_metrics = {}
 
     def calculate(self, results):
-        """Calculates average Sentence BERT similarity on all successfull
-        attacks.
+        """Calculates average Sentence BERT similarity on all successfull attacks.
+
         Args:
             results (``AttackResult`` objects):
                 Attack results for each instance in dataset
+
         Example::
+
+
             >> import textattack
             >> import transformers
             >> model = transformers.AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
@@ -50,19 +59,22 @@ class SBERTMetric(Metric):
             elif isinstance(result, SkippedAttackResult):
                 continue
             else:
-                self.original_candidates.append(result.original_result.attacked_text)
-                self.successful_candidates.append(result.perturbed_result.attacked_text)
+                self.original_candidates.append(
+                    result.original_result.attacked_text)
+                self.successful_candidates.append(
+                    result.perturbed_result.attacked_text)
 
         sbert_scores = []
         for c in range(len(self.original_candidates)):
             sbert_scores.append(
-                self.use_obj._sim_score(
+                self.model._sim_score(
                     self.original_candidates[c], self.successful_candidates[c]
                 ).item()
             )
 
-        self.all_metrics["avg_attack_sentence_bert_similarity"] = round(
-            sum(sbert_scores) / len(sbert_scores), 2
-        )
+        self.all_metrics["avg_sentence_bert_similarity"] = round(
+            sum(sbert_scores) / len(sbert_scores), 3
+        ) if len(sbert_scores) > 0 else 0.0
 
         return self.all_metrics
+
